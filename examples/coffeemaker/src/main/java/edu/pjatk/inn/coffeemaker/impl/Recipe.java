3ecd3e26cdd1d0e5a9f2b03f78254c103fe744d8
JustArchi <JustArchi@JustArchi.net>
2018-01-24 01:26:05 +0100

    H2

--- examples/coffeemaker/src/main/java/edu/pjatk/inn/coffeemaker/impl/Recipe.java
+++ examples/coffeemaker/src/main/java/edu/pjatk/inn/coffeemaker/impl/Recipe.java
@@ -28,12 +28,16 @@ public class Recipe implements Serializable {
     }
     
     /**
+	 * Returns amount of chocolate of this recipe.
+	 *
 	 * @return   Returns the amtChocolate.
 	 */
     public int getAmtChocolate() {
 		return amtChocolate;
 	}
     /**
+	 * Sets amount of chocolate of this recipe.
+	 *
 	 * @param amtChocolate   The amtChocolate to setValue.
 	 */
     public void setAmtChocolate(int amtChocolate) {
@@ -42,12 +46,16 @@ public class Recipe implements Serializable {
 		} 
 	}
     /**
+	 * Returns amount of coffee of this recipe.
+	 *
 	 * @return   Returns the amtCoffee.
 	 */
     public int getAmtCoffee() {
 		return amtCoffee;
 	}
     /**
+	 * Sets amount of coffee of this recipe.
+	 *
 	 * @param amtCoffee   The amtCoffee to setValue.
 	 */
     public void setAmtCoffee(int amtCoffee) {
@@ -56,12 +64,16 @@ public class Recipe implements Serializable {
 		} 
 	}
     /**
+	 * Returns amount of milk of this recipe.
+	 *
 	 * @return   Returns the amtMilk.
 	 */
     public int getAmtMilk() {
 		return amtMilk;
 	}
     /**
+	 * Sets amount of milk of this recipe.
+	 *
 	 * @param amtMilk   The amtMilk to setValue.
 	 */
     public void setAmtMilk(int amtMilk) {
@@ -70,12 +82,16 @@ public class Recipe implements Serializable {
 		} 
 	}
     /**
+	 * Returns amount of sugar of this recipe.
+	 *
 	 * @return   Returns the amtSugar.
 	 */
     public int getAmtSugar() {
 		return amtSugar;
 	}
     /**
+	 * Sets amount of sugar of this recipe.
+	 *
 	 * @param amtSugar   The amtSugar to setValue.
 	 */
     public void setAmtSugar(int amtSugar) {
@@ -84,12 +100,16 @@ public class Recipe implements Serializable {
 		} 
 	}
     /**
+	 * Returns the name of this recipe.
+	 *
 	 * @return   Returns the key.
 	 */
     public String getName() {
 		return name;
 	}
     /**
+	 * Sets the name of this recipe.
+	 *
 	 * @param name   The key to setValue.
 	 */
     public void setName(String name) {
@@ -98,29 +118,49 @@ public class Recipe implements Serializable {
     	}
 	}
     /**
+	 * Returns the price of this recipe.
+	 *
 	 * @return   Returns the price.
 	 */
     public int getPrice() {
 		return price;
 	}
     /**
+	 * Sets the price of this recipe.
+	 *
 	 * @param price   The price to setValue.
 	 */
     public void setPrice(int price) {
 		if (price >= 0) {
 			this.price = price;
 		} 
-	} 
+	}
+    /**
+	 * Compares this recipe to given one and returns a value indicating if it's the same recipe.
+	 *
+	 * @param r   The recipe to compare with.
+	 * @return   Returns the boolean value specifying if both recipes are the same.
+	 */
     public boolean equals(Recipe r) {
         if((this.name).equals(r.getName())) {
             return true;
         }
         return false;
     }
+    /**
+	 * Returns string representation of this recipe object.
+	 *
+	 * @return   Returns the string representation.
+	 */
     public String toString() {
     	return name;
     }
-
+    /**
+	 * Returns a recipe from given context.
+	 *
+	 * @param context   Context to create recipe from.
+	 * @return   Returns a new recipe object that matches given context data.
+	 */
 	static public Recipe getRecipe(Context context) throws ContextException {
 		Recipe r = new Recipe();
 		try {
@@ -135,7 +175,12 @@ public class Recipe implements Serializable {
 		}
 		return r;
 	}
-
+    /**
+	 * Returns a context from given recipe.
+	 *
+	 * @param recipe   Recipe to create context from.
+	 * @return   Returns a new context object that matches given recipe data.
+	 */
 	static public Context getContext(Recipe recipe) throws ContextException {
 		Context cxt = new ServiceContext();
 		cxt.putValue("key", recipe.getName());
