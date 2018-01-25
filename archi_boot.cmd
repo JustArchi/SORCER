2b3ad1b2d49a106443bd7d1d22ea4f122e561dbb
JustArchi <JustArchi@JustArchi.net>
2018-01-25 02:28:54 +0100

    Misc

--- archi_boot.cmd
+++ archi_boot.cmd
@@ -2,7 +2,6 @@
 pushd %~dp0
 set PATH=%PATH%;%~dp0_archi\Gradle\bin;%~dp0_archi\Ant\bin
 
-call gradle dist
 call gradle bootSorcer
 
 pause
---
+++ archi_dist.cmd
@@ -0,0 +1,7 @@
+@echo off
+pushd %~dp0
+set PATH=%PATH%;%~dp0_archi\Gradle\bin;%~dp0_archi\Ant\bin
+
+call gradle dist
+
+pause
